380c2407e051ab413b47953e597e39c0098492b8
white_devil_04200 <75082878+whitedevil0420@users.noreply.github.com>
2023-04-30 10:43:06 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Bootanimation_tree-for-A-B-devices
-Bootanimation magisk module for custom boot animation 
+this is a simple template of changing boot animation of your Android
+
+simply extract this zip and replace your bootanimation.zip in system golder and now compress all folders .
+
+don't touch other files
+
+now it become a magisk module that is ready to flash with magisk or twrp
+
+(recommended magisk )
+
+
+
+backup your current boot animation zip if ny error goes
+(i can tell there is no error you face)
+
+
+for any query
+dm me at teligram at @white_devil_0420
+   https://t.me/white_devil_0420
---
+++ config.sh
@@ -0,0 +1,64 @@
+
+# Magisk Module Tree Config Script
+# by whitedevil0420
+#replace your bootanimation.zip in system folder
+# Set to true if you need to enable Magic Mount
+# Most mods would like it to be enabled
+AUTOMOUNT=true
+
+# Set to true if you need to load system.prop
+PROPFILE=false
+
+# Set to true if you need post-fs-data script
+POSTFSDATA=false
+
+# Set to true if you need late_start service script
+LATESTARTSERVICE=false
+
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "            make ur own          "
+  ui_print "            boot animation        "
+  ui_print "         Boot Animation tree     "
+  ui_print "               by              "
+  ui_print "         whitedevil0420        "
+  ui_print "*******************************"
+}
+
+
+# List all directories you want to directly replace in the system
+# Check the documentations for more info about how Magic Mount works, and why you need this
+
+# This is an example
+REPLACE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+# Construct your own list here, it will override the example above
+# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
+REPLACE="
+/system/media/bootanimation.zip
+"
+
+
+
+set_permissions() {
+  
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
+
+  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
+  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
+  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
+
+  # The following is default permissions, DO NOT remove
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+}
+
+
+
